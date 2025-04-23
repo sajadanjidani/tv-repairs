bd7bf298aac5cd50c671005a195d164b1c67a3d3
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-23 15:46:41 +0330

    add : custome color in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html'],
+  content: ['./src/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        "center": true
+      },
+      colors:{
+        "Bg1": "#292F36",
+        "Bg2": "#1A1E23",
+        "Brand1": "#12F7D6",
+        "Brand2": "#98FAEC",
+        "Gray": "#43454D"
+      }
+    },
   },
   plugins: [],
 }
