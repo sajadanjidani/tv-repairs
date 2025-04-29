3d1b301a52d6ed3dc5326fbd7f7491e427e40c20
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-29 13:15:28 +0330

    coding : fix responsive about component

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -1,17 +1,17 @@
 const About = () => {
     return(
-        <div className="grid lg:flex w-full h-auto gap-10 py-10 md:py-0 md:h-screen bg-Bg2">
-            <div className="flex justify-center items-center w-full md:w-11/12 lg:w-1/3 h-full">
+        <div className="grid gap-16 md:gap-0 lg:flex w-full h-auto py-10 md:py-0 md:h-screen bg-Bg2">
+            <div className="flex justify-center items-center w-full md:w-11/12 lg:w-1/3 h-full mx-auto">
                 <div className="w-4/5 h-[260px] min-h-[260px] bg-green-700 overflow-hidden rounded-lg">
                     <img className="w-full h-full object-center object-cover" src="image/about/image.jpg" alt="TVrepairs"/>
                 </div>
             </div>
-            <div className="grid justify-center items-center w-full md:w-11/12 lg:w-2/3 h-full">
-                <div className="px-10 md:px-20">
+            <div className="grid justify-center items-center w-full md:w-11/12 lg:w-2/3 h-full mx-auto">
+                <div className="px-10 md:pl-20">
                     <span className="py-5 px-10 text-white text-xl bg-Bg1 border-4 border-Brand1 rounded-tl-[40px] rounded-br-[40px]">درباره من</span>
                     <div className="bg-Bg1 rounded-[40px] p-12">
                         <h1 className="text-Brand2">سلام !</h1>
-                        <p className="text-white leading-10">من <span className="text-Brand2">مازیار رحمانی</span> بیش از 10 سال است که در حوضه تعمیرات تلویزیون فعالیت دارم . خدمات بنده در محل فقط در تهران و کرج است و با ضمانت قطعات , قطعات اورجینال با گارانتی 3 تا 6 ماه کار خود را تضمین میکنم .</p>
+                        <p className="text-white leading-10 text-justify">من <span className="text-Brand2">مازیار رحمانی</span> بیش از 10 سال است که در حوضه تعمیرات تلویزیون فعالیت دارم . خدمات بنده در محل فقط در تهران و کرج است و با ضمانت قطعات , قطعات اورجینال با گارانتی 3 تا 6 ماه کار خود را تضمین میکنم .</p>
                     </div>
                 </div>
             </div>
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,9 +1,9 @@
 const Header = () => {
     return(
         <div className="grid w-full h-screen bg-Bg1">
-            <div className="container flex mt-16 w-10/12 max-h-[500px]">
+            <div className="container grid gap-5 lg:flex mt-16 w-10/12 max-h-[500px]">
                 {/* امار و ارقام */}
-                <div className="relative w-3/12 h-full">
+                <div className="hidden xl:block relative w-3/12 h-full">
                     <ul className="absolute bottom-0 grid text-white w-9/12 gap-10 py-14 px-10 bg-Bg2 rounded-[80px]">
                         <li>
                             <span><span className="text-Brand1">10</span> سال تجربه </span>
@@ -17,13 +17,13 @@ const Header = () => {
                     </ul>
                 </div>
                 {/* معرفی */}
-                <div className="grid w-5/12 h-full pt-20 px-14">
-                    <h1 className="text-5xl text-Brand2">تعمیرکار تلوزیون</h1>
+                <div className="grid w-full lg:w-8/12 xl:w-5/12 h-full py-0 lg:pt-20 px-0 lg:px-14">
+                    <h1 className="text-3xl pb-8 lg:text-5xl text-Brand2">تعمیرکار تلوزیون</h1>
                     <p className="text-lg leading-10 text-white">سلام <br/> من <span className="text-Brand2"> مازیار رحمانی </span> هستم <br/> تعمیرکار تلوزیونهای سونی , سامسونگ , ال جی و انواع برند های ایرانی و خارجی در سریع ترین زمان </p>
                 </div>
                 {/* عکس */}
-                <div className="flex item-center w-4/12 h-full">
-                    <div className="w-4/5 max-h-[500px] border-2 border-white rounded-tl-[200px] rounded-br-[200px] overflow-hidden">
+                <div className="flex item-center justify-center w-4/5 md:w-[500px] lg:w-4/12 h-4/5 md:h-full mx-auto min-w-[320px] min-h-[320px] pb-10 md:pb-0">
+                    <div className="lg:w-4/5 max-h-[500px] border-2 border-white rounded-lg md:rounded-tl-[200px] md:rounded-br-[200px] overflow-hidden">
                         <img className="w-full h-full object-center object-cover" src="image/header/photo.jpg"/>
                     </div>
                 </div>
